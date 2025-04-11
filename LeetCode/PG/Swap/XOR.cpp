c862a58749467bbef715d900692dbd6907ba44fc
Sandy2004 <134693152+sandeepa-oraon@users.noreply.github.com>
2025-04-11 11:18:35 +0530

    Create XOR.cpp

---
+++ LeetCode/PG/Swap/XOR.cpp
@@ -0,0 +1,14 @@
+// Swaping using XOR bit manipulation
+#include<iostream>
+int main() {
+    // std::cout << "Hello World!\n";
+    int a, b;
+    cin>>a;
+    cin>>b;
+    cout<<"before swapping, a:"<<a<<" b: "<<b<<endl; 
+    a= a ^ b;
+    b= a ^ b;
+    a= a ^ b;
+    cout<<"before swapping, a:"<<a<<" b: "<<b<<endl; 
+    
+}
