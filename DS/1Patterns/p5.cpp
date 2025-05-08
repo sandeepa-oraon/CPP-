217a35c5456c58c9f15bbc94383da5d482841d55
Sandeepa Oraon <sandeepa2004oraon@gmail.com>
2025-05-08 15:28:47 +0530

    v1

--- DS/1Patterns/p5.cpp
+++ DS/1Patterns/p5.cpp
@@ -1,18 +1,18 @@
 /*
-    *****
-    ****
-    ***
-    **
-    *
+    12345
+    1234
+    123
+    12
+    1
 */
 
 #include<iostream>
 
 using namespace std;
 int main(){
-    for(int i=0; i<5; i++){
-        for(int j=i; j<5; j++){
-            cout<<"*";
+    for(int i=5; i>0; i--){
+        for(int j=1; j<=i; j++){
+            cout<<j;
         }
         cout<<"\n";
     }
