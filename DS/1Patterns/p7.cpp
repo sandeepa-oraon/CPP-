2ee83194796e9b9488de090c8774f802c8cefb1a
Sandeepa Oraon <sandeepa2004oraon@gmail.com>
2025-05-12 14:51:27 +0530

    v1

--- DS/1Patterns/p7.cpp
+++ DS/1Patterns/p7.cpp
@@ -4,7 +4,17 @@ using namespace std;
 
 int main(){
     int n=5;
-    for(int i= n; i>0; i++){
-        for(int j=0; j<)
+    for(int i= n; i>0; i--){
+        for(int j=0; j<n-i; j++){
+            cout<<"  ";
+        }
+        for(int j=0; j<2*i-1; j++){
+            cout<<"* ";
+        }
+        for(int j=0; j<n-i; j++){
+            cout<<"  ";
+        }
+        cout<<endl;
     }
+    return 0;
 }
