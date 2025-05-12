0c06ac9aa24b80bf59535e1cbb030d1601f87f98
Sandeepa Oraon <sandeepa2004oraon@gmail.com>
2025-05-12 14:55:26 +0530

    v1

---
+++ DS/1Patterns/p8.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n= 5;
+    for(int i=0; i<n; i++){
+        for(int j=0; j< n-i-1; j++){
+            cout<<"  ";
+        }
+        for(int j=0; j< 2*i+1; j++){
+            cout<<" *";
+        }
+        for(int j=0; j< n-i-1; j++){
+            cout<<"  ";
+        }
+        cout<<"\n";
+    }
+    for(int i= n; i>0; i--){
+        for(int j=0; j<n-i; j++){
+            cout<<"  ";
+        }
+        for(int j=0; j<2*i-1; j++){
+            cout<<" *";
+        }
+        for(int j=0; j<n-i; j++){
+            cout<<"  ";
+        }
+        cout<<endl;
+    }
+}
