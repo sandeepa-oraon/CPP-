cc6249d371c283c1acc66a1ea4899fa46f69c94a
Sandeepa Oraon <sandeepa2004oraon@gmail.com>
2025-05-15 22:23:57 +0530

    v2

---
+++ DS/1Patterns/p14.cpp
@@ -0,0 +1,19 @@
+/*
+A 
+A B 
+A B C 
+A B C D 
+A B C D E 
+*/
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n=5;
+    for(int i=0; i<5; i++){
+        for(int j=5; j>0; j--)
+            cout<<"* ";
+    cout<<endl;
+    }
+}
--- DS/1Patterns/p5.cpp
+++ DS/1Patterns/p5.cpp
@@ -10,9 +10,15 @@
 using namespace std;
 int main(){
     int n=5;
-    for(int i=5; i>0; i--){
-        for(int j=0; j<i; j++){
-            cout<<" *";
+    // for(int i=5; i>0; i--){
+    //     for(int j=0; j<i; j++){
+    //         cout<<" *";
+    //     }
+    //     cout<<endl;
+    // }
+    for(int i=0; i<5; i++){
+        for (int j=0; j< (n-i); j++){
+            cout<<"* ";
         }
         cout<<endl;
     }
