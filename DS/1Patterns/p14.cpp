160162547243a5b7da4bfaaba2c0da97a404e2b0
Sandeepa Oraon <sandeepa2004oraon@gmail.com>
2025-05-15 22:52:51 +0530

    v2

--- DS/1Patterns/p14.cpp
+++ DS/1Patterns/p14.cpp
@@ -12,8 +12,8 @@ using namespace std;
 int main(){
     int n=5;
     for(int i=0; i<5; i++){
-        for(int j=5; j>0; j--)
-            cout<<"* ";
+        for(char j='A'; j<'A'+(n-i); j++)
+            cout<<j<<" ";
     cout<<endl;
     }
 }
