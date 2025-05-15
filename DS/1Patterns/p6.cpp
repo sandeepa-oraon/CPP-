16731e4bb452a574ee5364e140dd533712fa1aec
Sandeepa Oraon <sandeepa2004oraon@gmail.com>
2025-05-15 22:26:51 +0530

    v2

--- DS/1Patterns/p6.cpp
+++ DS/1Patterns/p6.cpp
@@ -1,19 +1,26 @@
 /*
-    12345
-    1234
-    123
-    12
-    1
+    1 2 3 4 5 
+    1 2 3 4 
+    1 2 3 
+    1 2 
+    1 
 */
 
 #include<iostream>
 
 using namespace std;
 int main(){
-    for(int i=5; i>0; i--){
-        for(int j=1; j<=i; j++){
-            cout<<j;
+    // for(int i=5; i>0; i--){
+    //     for(int j=1; j<=i; j++){
+    //         cout<<j;
+    //     }
+    //     cout<<"\n";
+    // }
+    int n=5;
+    for(int i=0; i<5; i++){
+        for(int j=0; j< (n-i); j++){
+            cout<< j+1<<" ";
         }
-        cout<<"\n";
+        cout<<endl;
     }
 }
