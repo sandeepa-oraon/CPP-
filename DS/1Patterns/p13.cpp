96ebe8ece1e1e95c9da7f39653fcf17fe35c29fe
Sandeepa Oraon <sandeepa2004oraon@gmail.com>
2025-05-17 09:59:12 +0530

    v1

---
+++ DS/1Patterns/p13.cpp
@@ -0,0 +1,19 @@
+/*
+1 
+2 3 
+4 5 6 
+7 8 9 10 
+11 12 13 14 15 
+*/
+#include<iostream>
+using namespace std;
+int main(){
+    int n=5, num=1;
+    for(int i=0; i<n; i++){
+        for(int j=0; j<=i; j++){
+            cout<<num<<" ";
+            num++;
+        }
+        cout<<endl;
+    }
+}
