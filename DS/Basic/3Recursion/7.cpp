2a96df0d6e2d53b99c2e0f7d9d9cc81c9b35ce22
Sandeepa Oraon <sandeepa2004oraon@gmail.com>
2025-05-19 23:40:45 +0530

    v1

--- DS/Basic/3Recursion/7.cpp
+++ DS/Basic/3Recursion/7.cpp
@@ -4,12 +4,25 @@
 
 #include<iostream>
 using namespace std;
-int reverse(int arr);
+int reverse(int arr[], int, int);
 int main(){
-    int arr[5]= {1, 2, 3, 4, 5};
-    int res= reverse(arr[5]);
+    int arr[]= {1,2,1,1,5,1};
+    int size= sizeof(arr)/sizeof(arr[0]);
+    int res= reverse(arr, 0, size-1);
+    cout<<"[";
+    for(int i=0; i<size; i++){
+        cout<<arr[i]<<" ";
+    }
+    cout<<"]";
 }
 
-int reverse(int arr){
-    for(int)
+int reverse(int arr[], int start, int end){
+    if(start >= end)
+        return 0;
+
+    int temp= arr[start];
+    arr[start]= arr[end];
+    arr[end]= temp;
+    reverse(arr, start+1, end-1);
+    return 0;
 }
