c0ef878d44d9d89a31b904d1466af623d5e1cc9d
Sandeepa Oraon <sandeepa2004oraon@gmail.com>
2025-05-19 23:12:35 +0530

    v1

---
+++ DS/Basic/3Recursion/7.cpp
@@ -0,0 +1,15 @@
+/*
+7. Reverse an array
+*/
+
+#include<iostream>
+using namespace std;
+int reverse(int arr);
+int main(){
+    int arr[5]= {1, 2, 3, 4, 5};
+    int res= reverse(arr[5]);
+}
+
+int reverse(int arr){
+    for(int)
+}
